841f65c46ef67644420358b0ca789c529a40a31c
Nura730 <arunshanmugavel3726@gmail.com>
2026-06-05 08:19:24 +0530

    deleted frontend for creating the UI again completly differnet

--- firo-frontend/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import AppRoutes from "./routes";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-[#050212] text-slate-100 antialiased flex flex-col font-sans">
-      <AppRoutes />
-    </div>
-  );
-}
